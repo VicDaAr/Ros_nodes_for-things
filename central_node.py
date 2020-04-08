ba5d0dd311c5cfa0c5f1e90988826fc2dfd4e0d4
Daisuke <daisukearaki@hotmail.com>
2020-04-08 18:06:35 -0300

    invesalius_node just a break

--- central_node.py
+++ central_node.py
@@ -2,6 +2,8 @@
 
 import rospy
 from std_msgs.msg import String
+import std_msgs
+
 
 class Central_node:
     def __init__(self):
@@ -17,20 +19,30 @@ class Central_node:
         #Publishers
         #pub_... = rospy.Publisher('Topic', String, queue_size=10)
 
+        self.inV_pub = rospy.Publisher('iiwa_coordinates', String, queue_size=10) #send iiwa coordinates in the iiwa_coordinates topic
+
         #END - Publishers-------------------------------------------------
 
         #Subscribers
         #sub_... = rospy.Subscriber("Topic", String, self.callback)
-        sub_test = rospy.Subscriber('connection', String, self.callback_test)
+
+        self.sub_test = rospy.Subscriber('connection', String, self.test_callback)
+
         #END - Subscribers------------------------------------------------
 
     #callbacks for subscribers
     def callback(self,data): #data is the messege subscriber received
         return
 
-    def callback_test(self,data):
-        print(data)
+    def test_callback(self,data):
+        print(data.data)
     #END - Callback---------------------------------------------------
+
+    #publish function for publishers
+    def inV_publish(self, message):
+        self.inV_pub.publish(message)
+
+    #END - publish-----------------------------------------------------
 
 if __name__ == '__main__':
     c_node = Central_node()
