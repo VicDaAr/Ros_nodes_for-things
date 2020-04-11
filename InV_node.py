709e9803aad933c82cf991e7032ae3c7679a9351
Daisuke <daisukearaki@hotmail.com>
2020-04-10 22:13:43 -0300

    sending_node finished and working

--- InV_node.py
+++ InV_node.py
@@ -8,20 +8,20 @@ import std_msgs
 class InV_node:
     def __init__(self):
         #variables
-        self.received_coordinates = ([None,None,None,None,None,None])
+        self.received_coordinates = ''
 
         #END - variables ----------------------------------------------
 
         #init_node
-        rospy.init_node('central_node', anonymous=False)
-        rate = rospy.Rate(10)  # 100hz update rate.
+        rospy.init_node('inV_node', anonymous=False)
+        rate = rospy.Rate(10)  # 10hz update rate.
         #END - init_node--------------------------------------------------
 
 
         #Subscribers
         #sub_... = rospy.Subscriber("Topic", String, self.callback)
 
-        self.inV_sub = rospy.Subscriber('iiwa_coordinates', String, self.save_value)
+        self.inV_sub = rospy.Subscriber('coordinates_inV', String, self.save_value)
 
         #END - Subscribers------------------------------------------------
 
@@ -30,11 +30,12 @@ class InV_node:
         return
 
     def save_value(self,data): #Temporary solution, immediately send message to inV_Node
-        data.data
+        self.received_coordinates = data.data
+        print(self.received_coordinates)
 
     #END - Callback---------------------------------------------------
 
 
 if __name__ == '__main__':
-    c_node = InV_node()
+    i_node = InV_node()
     rospy.spin()
